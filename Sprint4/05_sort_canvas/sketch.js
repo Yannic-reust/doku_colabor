072cadc35b9f461e75a0bf17b06f66b3583cec0e
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2024-06-03 10:11:45 +0200

    feat: rename

---
+++ Sprint4/05_sort_canvas/sketch.js
@@ -0,0 +1,41 @@
+let img;
+
+function preload() {
+  img = loadImage("image.jpg");
+}
+
+function setup() {
+  createCanvas(500, 500);
+
+  img.resize(100, 100);
+
+  noSmooth();
+}
+
+function draw() {
+  img.loadPixels();
+
+  for (let i = 0; i < 1000; i++) {
+    sortPixels();
+  }
+
+  img.updatePixels();
+
+  image(img, 0, 0, width, height);
+}
+
+function sortPixels() {
+  const x = random(img.width - 1); // Ensuring we don't go out of bounds
+  const y = random(img.height);
+
+  const colorOne = img.get(x, y);
+  const colorTwo = img.get(x + 1, y); // Compare with the next pixel to the right
+
+  const totalOne = red(colorOne) + green(colorOne) + blue(colorOne);
+  const totalTwo = red(colorTwo) + green(colorTwo) + blue(colorTwo);
+
+  if (totalOne < totalTwo) {
+    img.set(x, y, colorTwo);
+    img.set(x + 1, y, colorOne);
+  }
+}
