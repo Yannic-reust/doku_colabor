23a98daf44910f4c58e97707ca9d975b4dce09ec
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2024-06-03 10:30:20 +0200

    feat working

--- Sprint4/05_sort_canvas/sketch.js
+++ Sprint4/05_sort_canvas/sketch.js
@@ -7,9 +7,12 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(200, 200);
   img.resize(100, 100);
   noSmooth();
+
+  // Draw the image on the canvas initially
+  image(img, 0, 0, width, height);
 
   // Create a button and attach the triggerDraw function to its mousePressed event
   button = createButton("Process Image");
@@ -19,33 +22,34 @@ function setup() {
 
 function draw() {
   if (shouldDraw) {
-    img.loadPixels();
+    loadPixels(); // Load the canvas pixels
 
     for (let i = 0; i < 1000; i++) {
       sortPixels();
     }
 
-    img.updatePixels();
-    image(img, 0, 0, width, height);
+    updatePixels(); // Update the canvas with modified pixels
   }
 }
 
 function triggerDraw() {
+  console.log("Button pressed");
   shouldDraw = true;
 }
 
 function sortPixels() {
-  const x = floor(random(img.width - 1)); // Ensuring we don't go out of bounds
-  const y = floor(random(img.height));
+  const x = floor(random(width - 1)); // Ensuring we don't go out of bounds
+  const y = floor(random(height));
 
-  const colorOne = img.get(x, y);
-  const colorTwo = img.get(x + 1, y); // Compare with the next pixel to the right
+  const colorOne = get(x, y);
+  const colorTwo = get(x + 1, y);
 
+  //console.log(colorOne);
   const totalOne = red(colorOne) + green(colorOne) + blue(colorOne);
   const totalTwo = red(colorTwo) + green(colorTwo) + blue(colorTwo);
 
   if (totalOne < totalTwo) {
-    img.set(x, y, colorTwo);
-    img.set(x + 1, y, colorOne);
+    set(x, y, colorTwo);
+    set(x + 1, y, colorOne);
   }
 }
